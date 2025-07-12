4d3d7055aa63c4d6e460425608c7f2b9127ebff6
mariacgs <mbcamposg@gmail.com>
2025-07-12 20:26:40 +0200

    Refactor transmission time calculation and improve message timing handling in formatMatcher

--- src/formatMatcher.py
+++ src/formatMatcher.py
@@ -3,11 +3,16 @@ import os
 
 def transmissionTime(baseDelay, numBytes, bandwidth):
     numBits = numBytes*8
-    bandwidth = bandwidth * 10e3
-    return baseDelay + (numBits/bandwidth)
+    bandwidthBitsPerSec = bandwidth * 1e9
+    delaySec = numBits/bandwidthBitsPerSec
+    return baseDelay + (delaySec*1e6)
 
 def main(vefFile, packets):
     baseDelay = 1.5
+    simTime = 0
+    startTime = 0
+
+    """ CHANGE TO USER SPECIFIED"""
     bandwidth = 200
 
     listDicts = []
@@ -19,9 +24,10 @@ def main(vefFile, packets):
             #check if its not a wait
             if i[3] != 0 and i[1] != i[2]:
                 numBytes = int(i[3])
-                timeMicro = int(i[5])*10e-6
+                timeMicro = int(i[5])*1e-6*1000 # multiply by a 1000 because we get how many picoseconds a cycle takes: 1000ps
                 msgID = int(i[0])
-                #depTime = int(i[6])
+                dTime = int(i[6])
+                dep = int(i[4])
 
                 #reset dictionary
                 dict = {"A" : 0, "B": 0, "t": "", "d": 0}
@@ -32,10 +38,10 @@ def main(vefFile, packets):
                 dict["d"] = numBytes
 
                 #check if its a message that depends on another message
-                if i[4] == "0" or i[4] == "4":
+                if dep == 0 or dep == 4:
 
                     #adding time to dict for independent messages, in microseconds
-                    dict["t"] = round(timeMicro, 4)
+                    dict["t"] = round(timeMicro, 5)
 
                     #adding time(in microseconds) and message ID to mapping
                     msgTimeMapping[msgID] = timeMicro
@@ -44,26 +50,26 @@ def main(vefFile, packets):
                 else:
 
                     #if its a receive dependency:
-                    if i[4] == "2" or i[4] == "6":
+                    if dep == 2 or dep == 6:
 
                         #get transmission time of the message
                         transTime = transmissionTime(baseDelay, numBytes, bandwidth)
 
                         #get the timestamp it was received considering send and transmission time
-                        recvTime = msgTimeMapping[int(i[6])] + transTime
+                        recvTime = msgTimeMapping[dTime] + transTime
 
                         #add send timestamp of message in microseconds to the dictionary
-                        dict["t"] = round(timeMicro + recvTime, 4)
+                        dict["t"] = round(timeMicro + recvTime, 5)
 
                         #adding timestamp (in microseconds) and message ID to mapping
                         msgTimeMapping[msgID] = timeMicro + recvTime
 
                     else:
                         #get the timestamp the message depends on
-                        depTime = msgTimeMapping[int(i[6])]
+                        depTime = msgTimeMapping[dTime]
 
                         #add send timestamp of message in microseconds to the dictionary
-                        dict["t"] = round(timeMicro + depTime, 4)
+                        dict["t"] = round(timeMicro + depTime, 5)
 
                         #adding timestamp (in microseconds) and message ID to mapping
                         msgTimeMapping[msgID] = timeMicro + depTime
@@ -73,7 +79,8 @@ def main(vefFile, packets):
     with open(packets, mode ="w") as f:
         json.dump(listDicts, f, indent=2)
 
+
     #print(listDicts)
-    #print(msgTimeMapping)
+    print(msgTimeMapping)
 
 main("data/output_trace_dummyRecv.vef", "packets.yaml")
